8a244b915a015f27012066892ac20a20203e9420
nikishkaa <nikita.goutor@gmail.com>
2024-05-20 14:50:45 +0300

    Update Ship.java.

--- src/by/itstep/goutor/threads/port/model/logic/Ship.java
+++ src/by/itstep/goutor/threads/port/model/logic/Ship.java
@@ -1,4 +1,12 @@
 package by.itstep.goutor.threads.port.model.logic;
 
+import java.io.PrintStream;
+
 public class Ship {
+    private int containerSize; // max кол-во контейнеров на корабле
+    private int ShipId; // id корабля
+    private PrintStream stream; // для записи UI
+
+    private Thread thread;
+    private boolean running;
 }
